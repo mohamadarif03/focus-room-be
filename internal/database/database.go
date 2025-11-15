76dd6e4361f25e6a92bd0b06e436d81a7def513e
mohamadarif03 <pecintarealmadrid2@gmail.com>
2025-11-15 13:52:29 +0700

    Reapply "refactort: db conncection"
    This reverts commit 4ac78285a9a3325d3fefd32fd0efb720b57ab9c6.

--- internal/database/database.go
+++ internal/database/database.go
@@ -12,16 +12,25 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	sslmode := os.Getenv("DB_SSLMODE")
+	var dsn string
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Shanghai",
-		host, user, password, dbname, port, sslmode)
+	databaseURL := os.Getenv("DATABASE_URL")
 
+	if databaseURL != "" {
+		dsn = databaseURL
+		log.Println("Menggunakan DATABASE_URL (mode Railway/Produksi)")
+	} else {
+		log.Println("DATABASE_URL tidak ditemukan, merakit DSN dari .env (mode Lokal)")
+		host := os.Getenv("DB_HOST")
+		port := os.Getenv("DB_PORT")
+		user := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASSWORD")
+		dbname := os.Getenv("DB_NAME")
+		sslmode := os.Getenv("DB_SSLMODE")
+
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Shanghai",
+			host, user, password, dbname, port, sslmode)
+	}
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
