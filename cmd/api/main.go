579ab739ac5af4d71742b40c58e21244aefa3b01
mohamadarif03 <pecintarealmadrid2@gmail.com>
2025-12-05 13:29:11 +0700

    Update main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -23,19 +23,19 @@ func main() {
 		log.Println("Info: File .env tidak ditemukan. Menggunakan System Environment Variables (Railway/Docker).")
 	}
 	database.InitDB()
-	database.DB.Migrator().DropTable(
-		&model.User{}, &model.Task{}, &model.Material{}, &model.Package{}, &model.Quiz{},
-		&model.QuizQuestion{}, &model.QuizAttempt{},
-		&model.QuizAttemptDetail{}, &model.FocusSession{},
-		&model.QuizLog{}, &model.MaterialChat{},
-	)
-
-	// database.DB.AutoMigrate(
+	// database.DB.Migrator().DropTable(
 	// 	&model.User{}, &model.Task{}, &model.Material{}, &model.Package{}, &model.Quiz{},
 	// 	&model.QuizQuestion{}, &model.QuizAttempt{},
 	// 	&model.QuizAttemptDetail{}, &model.FocusSession{},
 	// 	&model.QuizLog{}, &model.MaterialChat{},
 	// )
+
+	database.DB.AutoMigrate(
+		&model.User{}, &model.Task{}, &model.Material{}, &model.Package{}, &model.Quiz{},
+		&model.QuizQuestion{}, &model.QuizAttempt{},
+		&model.QuizAttemptDetail{}, &model.FocusSession{},
+		&model.QuizLog{}, &model.MaterialChat{},
+	)
 
 	geminiAPIKey := os.Getenv("GEMINI_API_KEY")
 	database.Seed()
